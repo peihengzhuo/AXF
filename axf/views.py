d61f7e343b6b512c84c2af14d543d06ea75d51d8
phz <1974586186@qq.com>
2018-10-31 06:07:12 -0700

    初步搭建axf

---
+++ axf/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return None
+
+
+def home(request):
+    return None
+
+
+def market(request):
+    return None
+
+
+def cart(request):
+    return None
+
+
+def mine(request):
+    return None
