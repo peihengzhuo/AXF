d87e0e93f52c016623ac9db89428b8248aa5c707
phz <1974586186@qq.com>
2018-11-02 23:06:49 -0700

    部分功能完成

--- axf/models.py
+++ axf/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# 基础类
 class Base(models.Model):
-    # 图片
     img = models.CharField(max_length=100)
-    # 名称
     name = models.CharField(max_length=100)
-    # 商品编号
     trackid = models.CharField(max_length=10)
 
     class Meta:
@@ -27,5 +24,78 @@ class Nav(Base):
 class Mustbuy(Base):
     class Meta:
         db_table = 'axf_mustbuy'
+
+
+# 商品部分
+class Shop(Base):
+    class Meta:
+        db_table = 'axf_shop'
+
+
+
+# 商品主体内容
+class MainShow(models.Model):
+    trackid = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=8)
+    brandname = models.CharField(max_length=50)
+
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=8)
+    productid1 = models.CharField(max_length=8)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.FloatField()
+    marketprice1 = models.FloatField()
+
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=8)
+    productid2 = models.CharField(max_length=8)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.FloatField()
+    marketprice2 = models.FloatField()
+
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=8)
+    productid3 = models.CharField(max_length=8)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.FloatField()
+    marketprice3 = models.FloatField()
+
+    class Meta:
+        db_table = 'axf_mainshow'
+
+
+
+# 商品分类
+class Foodtypes(models.Model):
+    typeid = models.CharField(max_length=8)
+    typename = models.CharField(max_length=100)
+    childtypenames = models.CharField(max_length=256)
+    typesort = models.IntegerField()
+    class Meta:
+        db_table = 'axf_foodtypes'
+
+
+# 商品信息
+class Goods(models.Model):
+    productid = models.CharField(max_length=10)
+    productimg = models.CharField(max_length=100)
+    productname = models.CharField(max_length=100)
+    productlongname = models.CharField(max_length=100)
+    isxf = models.BooleanField(default=False)
+    pmdesc = models.BooleanField(default=False)
+    specifics = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    marketprice = models.DecimalField(max_digits=7, decimal_places=2)
+    categoryid= models.IntegerField()
+    childcid = models.IntegerField()
+    childcidname = models.CharField(max_length=100)
+    dealerid = models.CharField(max_length=10)
+    storenums = models.IntegerField()
+    productnum = models.IntegerField()
+
+    class Meta:
+        db_table = 'axf_goods'
 
 
--- axf/views.py
+++ axf/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+from axf.models import Wheel, Nav, Mustbuy, Shop, MainShow
+
 
 def home(request):
-    return render(request, 'home/home.html')
+    # 首页
+    wheels = Wheel.objects.all()
+    navs = Nav.objects.all()
+    mustbuys = Mustbuy.objects.all()
+
+    # 商品部分
+    shopList = Shop.objects.all()
+    shophead = shopList[0]
+    shoptab = shopList[1:3]
+    shopclass = shopList[3:7]
+    shopcommend = shopList[7:11]
+
+    # 商品主体内容
+    mainshows = MainShow.objects.all()
+    data = {
+        'wheels':wheels,
+        'navs':navs,
+        'mustbuys': mustbuys,
+        'shophead': shophead,
+        'shoptab': shoptab,
+        'shopclass': shopclass,
+        'shopcommend': shopcommend,
+        'mainshows': mainshows
+
+    }
+
+    return render(request, 'home/home.html',context=data)
 
 
 def market(request):
